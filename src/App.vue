462ff05cb7f6a414617936e973bf15ae4533e661
Justin Zhang <justinzhang@Justins-MacBook-Pro.local>
2020-10-03 18:09:28 -0400

    adding code

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <swipeable-cards />
+  </div>
+</template>
+
+<script>
+import SwipeableCards from './components/SwipeableCards.vue'
+
+export default {
+  name: 'app',
+  components: { SwipeableCards }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  z-index: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
